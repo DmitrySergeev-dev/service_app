93ff7dd64e8e0188df7287d4b57f25f4f1612386
Dmitry Sergeev <dimonsergeev10@mail.ru>
2023-01-21 15:24:28 +0300

    Added DRF View API

--- service/service/settings.py
+++ service/service/settings.py
@@ -35,6 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom apps
+    'clients',
+    'services',
 ]
 
 MIDDLEWARE = [
